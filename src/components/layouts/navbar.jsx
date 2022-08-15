af22523e1d826ff08820cd5c588b7bb30ce57cbb
Prince Sylvanus <princehsylvanus@gmail.com>
2022-08-15 09:59:54 +0100

    Navbar component created

--- src/components/layouts/navbar.jsx
+++ src/components/layouts/navbar.jsx
@@ -3,7 +3,38 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    
+     <nav>
+      <div>
+      <ul>
+        <li>Sell on Jumia</li>
+      </ul>
+      </div>
+
+<div>
+
+
+      <ul>
+        <li>Jumia</li>
+      </ul>
+      <ul>
+        <li>Pay</li>
+      </ul>
+      <ul>
+        <li>Food </li>
+      </ul>
+     
+      <ul>
+        <li>Party </li>
+      </ul>
+     
+      <ul>
+        <li>Prime </li>
+      </ul>
+      <ul>
+        <li>Logistics </li>
+      </ul>
+      </div>
+     </nav>
     </>
   )
 }
