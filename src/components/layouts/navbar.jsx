240b4cf7e40ef19d07815f13f2ba6e6fd1a6de8f
Sylvanus Prince <princehsylvanus@gmail.com>
2022-08-17 00:01:41 +0100

    Navbar init

--- src/components/layouts/navbar.jsx
+++ src/components/layouts/navbar.jsx
@@ -1,42 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import "../../styles/navbar.css";
 const Navbar = () => {
   return (
     <>
-     <nav>
-      <div>
-      <ul>
-        <li>Sell on Jumia</li>
-      </ul>
-      </div>
+      <nav className="navBox">
+        <div>
+          <ul>
+            <li ><a href="#" className="top">Sell on Jumia</a></li>
+          </ul>
+        </div>
 
-<div>
+        <div className="navItem">
+          <ul>
+            <li><a href="#" className="navItemLink"> JUMIA</a></li>
+          </ul>
+          <ul>
+            <li> <a href="#" className="navItemLink"> PARTY </a></li>
+          </ul>
+          <ul>
+            <li> <a href="#" className="navItemLink"> FOOD </a> </li>
+          </ul>
 
+          <ul>
+            <li> <a href="#" className="navItemLink"> PARTY </a></li>
+          </ul>
 
-      <ul>
-        <li>Jumia</li>
-      </ul>
-      <ul>
-        <li>Pay</li>
-      </ul>
-      <ul>
-        <li>Food </li>
-      </ul>
-     
-      <ul>
-        <li>Party </li>
-      </ul>
-     
-      <ul>
-        <li>Prime </li>
-      </ul>
-      <ul>
-        <li>Logistics </li>
-      </ul>
-      </div>
-     </nav>
+          <ul>
+            <li><a href="#" className="navItemLink"> PRIME </a></li>
+          </ul>
+          <ul>
+            <li> <a href="#" className="navItemLink"> LOGISTICS</a> </li>
+          </ul>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
