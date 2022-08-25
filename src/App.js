73e94479995436ccf61e31638104c18fbfabb45f
Sylvanus Prince <princehsylvanus@gmail.com>
2022-08-25 21:01:38 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/layouts/navbar';
 function App() {
   return (
     // <div className="App">
     <div>
+   
+
+
+    <Router>
     <Navbar />
+    <Routes>
+        <Route  path='/' exact element={<index />} />
+        {/* <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/blog' element={<Blog/>} />
+        <Route path='/giving' element={<Giving />} />
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path='/usestate' element={<Usestate />} />
+        <Route path='/article' element={<Article />} /> */}
+    </Routes>
+    </Router>
     </div>
    
   //  </div>
--- src/components/layouts/navbar.jsx
+++ src/components/layouts/navbar.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import "../../styles/navbar.css";
+import { GiStarFlag, GiStarAltar } from 'react-icons/gi';
 const Navbar = () => {
   return (
     <>
       <nav className="navBox">
         <div>
           <ul>
-            <li ><a href="#" className="top">Sell on Jumia</a></li>
+            <li ><a href="#" className="top"> <span><GiStarFlag fill="#0033fe"/></span> <span>Sell on Jumia</span></a></li>
           </ul>
         </div>
 
         <div className="navItem">
           <ul>
-            <li><a href="#" className="navItemLink"> JUMIA</a></li>
+            <li><a href="#" className="navItemLink"> <span><GiStarFlag fill="#0033fe"/></span>JUMIA</a></li>
           </ul>
           <ul>
-            <li> <a href="#" className="navItemLink"> PARTY </a></li>
+            <li> <a href="#" className="navItemLink"> <span>< GiStarAltar fill="red"/></span>PARTY </a></li>
           </ul>
           <ul>
-            <li> <a href="#" className="navItemLink"> FOOD </a> </li>
-          </ul>
-
-          <ul>
-            <li> <a href="#" className="navItemLink"> PARTY </a></li>
+            <li> <a href="#" className="navItemLink"> <span>IMG</span>FOOD </a> </li>
           </ul>
 
           <ul>
-            <li><a href="#" className="navItemLink"> PRIME </a></li>
+            <li> <a href="#" className="navItemLink"><span>IMG</span>PARTY </a></li>
+          </ul>
+
+          <ul>
+            <li><a href="#" className="navItemLink"> <span>IMG</span>PRIME </a></li>
           </ul>
           <ul>
-            <li> <a href="#" className="navItemLink"> LOGISTICS</a> </li>
+            <li> <a href="#" className="navItemLink"> <span>IMG</span>LOGISTICS</a> </li>
           </ul>
         </div>
       </nav>
